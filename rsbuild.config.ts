283aa460554047856ba3823bdd1409074a24c76d
Muhammad Yusuf <yusuf@iamyuu.dev>
2024-12-20 12:27:31 +0700

    chore: setup vanilla-extract

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { VanillaExtractPlugin } from '@vanilla-extract/webpack-plugin';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact({
+      reactRefreshOptions: {
+        exclude: [/\.css\.ts$/],
+      },
+    }),
+  ],
+  tools: {
+    rspack: {
+      plugins: [new VanillaExtractPlugin()],
+    },
+  },
 });
