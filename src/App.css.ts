59e7e889d96cc80b6b4efc572395f96a55e228c7
Muhammad Yusuf <yusuf@iamyuu.dev>
2024-12-20 13:13:38 +0700

    feat: example responsive using vanilla-extract

--- src/App.css.ts
+++ src/App.css.ts
@@ -1,5 +1,12 @@
 import { globalStyle, style } from '@vanilla-extract/css';
 
+const responsiveStyle = ({ tablet = {}, desktop = {} }) => ({
+  '@media': {
+    'screen and (min-width: 768px)': tablet,
+    'screen and (min-width: 1024px)': desktop
+  }
+});
+
 globalStyle('body', {
   margin: 0,
   color: '#fff',
@@ -7,7 +14,7 @@ globalStyle('body', {
   backgroundImage: 'linear-gradient(to bottom, #020917, #101725)',
 });
 
-export const content = style({
+export const content = style([{
   display: 'flex',
   minHeight: '100vh',
   lineHeight: '1.1',
@@ -18,10 +25,17 @@ export const content = style({
   'h1': {
     fontSize: '3.6rem',
     fontWeight: 700,
+    margin: '0',
   },
   'p': {
     fontSize: '1.2rem',
     fontWeight: 400,
     opacity: 0.5,
   },
-});
+},
+responsiveStyle({
+  tablet: {
+    flexDirection: 'column-reverse',
+  },
+})
+]);
